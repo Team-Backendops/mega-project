033c6cffe0560bfd264d6448c6ad253b10deb748
kumbharprath <142964670+kumbharprath@users.noreply.github.com>
2024-09-01 18:17:52 +0530

    Update service.py

--- backend/models/service.py
+++ backend/models/service.py
@@ -1,19 +1,6 @@
 from pydantic import BaseModel, EmailStr
 from typing import List, Optional
 
-class ServiceProvider(BaseModel):
-    name: str
-    service_offered: str
-    description: str
-    location: dict
-    contact_info: dict
-    operating_hours: Optional[str] = None
-    website: Optional[str] = None
-    ratings: Optional[float] = None
-    profile_image_id: Optional[str] = None
-    adhaar_card_image_id: Optional[str] = None
-    pan_card_image_id: Optional[str] = None
-    office_images_ids: Optional[List[str]] = None
 
 class Location(BaseModel):
     address: str
@@ -24,3 +11,13 @@ class Location(BaseModel):
 class ContactInfo(BaseModel):
     phone_number: str
     email: EmailStr
+
+class ServiceProvider(BaseModel):
+    name: str
+    service_offered: str
+    description: str
+    location: Location
+    contact_info: ContactInfo
+    operating_hours: Optional[str] = None
+    website: Optional[str] = None
+    ratings: Optional[float] = None
