4e9ac89307278ec40a81b17a18b446a3f94a6f63
kumbharprath <142964670+kumbharprath@users.noreply.github.com>
2024-09-10 21:12:51 +0530

    Update config.py

--- backend/core/config.py
+++ backend/core/config.py
@@ -3,6 +3,8 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     mongo_url: str = "mongodb://localhost:27017"
     database_name: str = "service_listing_db"
+    SECRET_KEY: str= "my_secret_key_1234567890"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     class Config:
         env_file = ".env"
